bfb4f8d0635dfdc1160c6c847050910170106bcd
jake <jakebs540@gmail.com>
2021-12-15 16:01:08 +0000

    user can add transactions

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const balance = document.getElementById('balance')
 const moneyPlus = document.getElementById('money-plus')
 const moneyMinus = document.getElementById('money-minus')
 const list = document.getElementById('list')
-const form = document.getElementById('forms')
+const form = document.getElementById('form')
 const text = document.getElementById('text')
 const amount = document.getElementById('amount')
 
@@ -11,10 +11,39 @@ const dummyTransactions = [
   { id: 2, text: 'Salary', amount: 250 },
   { id: 3, text: 'Books', amount: -10 },
   { id: 4, text: 'Lotto', amount: 1000 },
-  { id: 5, text: 'Dinner', amount: -120 },
-]
+  { id: 5, text: 'Dinner', amount: -120 }
+];
 
 let transactions = dummyTransactions;
+
+// Add transaction
+function addTransaction(e) {
+  e.preventDefault();
+
+  if (text.value.trim() === '' || amount.value.trim() === '') {
+    alert('Please add a text and amount');
+  } else {
+    const transaction = {
+      id: generateID(),
+      text: text.value,
+      amount: +amount.value
+    };
+
+    transactions.push(transaction);
+
+    AddtransactionDOM(transaction);
+
+    updateValues();
+
+    text.value = '';
+    amount.value = '';
+  }
+}
+
+// Generate random ID
+function generateID() {
+  return Math.floor(Math.random() * 100000);
+}
 
 // Add transactions to DOM list
 function AddtransactionDOM(transaction) {
@@ -63,3 +92,5 @@ function init() {
 }
 
 init();
+
+form.addEventListener('submit', addTransaction);
